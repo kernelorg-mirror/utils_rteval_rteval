a00fc8a26476f25361e5d40c69dedc063be578d3
John Kacur <jkacur@redhat.com>
2024-11-14 15:45:08 -0500

    Create rteval-3.9
    Signed-off-by: John Kacur <jkacur@redhat.com>

--- rteval/version.py
+++ rteval/version.py
@@ -2,6 +2,7 @@
 #
 #   Copyright 2015  Clark Williams <williams@redhat.com>
 #   Copyright 2012 - 2013   David Sommerseth <davids@redhat.com>
+#   Copyright 2024  John Kacur <jkacurWredhat.com>
 #
 
-RTEVAL_VERSION = '3.8'
+RTEVAL_VERSION = '3.9'
